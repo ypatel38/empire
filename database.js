6dcbf3fd9dd068e4f7bd73bbf7d174d6ddb570a1
Hiran <hiran.bomm@gmail.com>
2020-04-03 22:04:47 -0500

    started db stuff

--- database.js
+++ database.js
@@ -7,4 +7,28 @@ class Database {
   }
 }
 
+
+
+const sqlite3 = require('sqlite3').verbose();  // set to verbose for stack traces. change if it gets annoying
+
+// open up database in memory
+let db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+
+
+/*
+// close database connection down
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+}); 
+*/
+
 module.exports = Database
